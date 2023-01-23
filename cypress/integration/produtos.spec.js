527e99b7c2057aecb812f7303b6f6e218db6a09e
milenaggoes <rm94305@fiap.com.br>
2023-01-23 11:58:19 -0300

    Get e Post

--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -4,10 +4,28 @@ describe('Testes da Funcionalidade Produtos', () => {
     it('Listar produtos', () => {
         cy.request({
             method: 'GET',
-            url: 'http://localhost:3000/produtos'
+            url: 'produtos'
         }).then((response)=>{
             expect(response.body.produtos[0].nome).to.equal('Logitech MX Vertical')
             expect(response.status).to.equal(200)
+            expect(response.body).to.have.property('produtos')
+            expect(response.duration).to.be.lessThan(20)
+        })
+    });
+
+    it('Cadastrar produto', () => {
+        cy.request({
+            method: 'POST',
+            url: 'produtos',
+            body: {
+                "nome": "Produto EBAC NOVO",
+                "preco": 200,
+                "descricao": "Produto novo",
+                "quantidade": 100
+            }
+        }).then((response)=>{
+            expect(response.status).to.equal(201)
+            expect(response.body.message).to.equal('Cadastro realizado com sucesso')
         })
     });
 });
